0be416e4c2d2ad7b6adf88d75e82fbc91e86d667
Sergey T <forl86@gmail.com>
2024-08-04 04:01:26 +0800

    second commmit

---
+++ src/main/java/hiber/dao/UserDao.java
@@ -0,0 +1,12 @@
+package hiber.dao;
+
+import hiber.model.User;
+import org.hibernate.SessionFactory;
+
+import java.util.List;
+
+public interface UserDao {
+   void add(User user);
+   List<User> listUsers();
+   SessionFactory getSessionFactory();
+}
